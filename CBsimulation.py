72a28b04073b6e03272dffba9b1b1b4f931ed100
gklabs <giridharkaushikr@gmail.com>
2020-11-22 14:24:40 -0500

    added poisson arrivals

--- CBsimulation.py
+++ CBsimulation.py
@@ -2,10 +2,13 @@
 import random
 import numpy as np
 import scipy
+import bisect
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class customer:
-	def __init__(self,cust_id,cust_type,bike,start_time, end_time,duration,start_station_id,end_station_id ):
+	def __init__(self,cust_id,cust_type,bike,start_time, end_time,duration,start_station_id,end_station_id,satisfaction ):
 		self.cust_id= cust_id
 		self.cust_type= cust_type
 		self.bike = bike #assigns the bike object to the customer
@@ -14,6 +17,7 @@ class customer:
 		self.duration=duration
 		self.start_station_id= start_station_id
 		self.end_station_id= end_station_id
+		self.satisfaction = satisfaction #0 means satisfied, 1 means dissatisfied
 		
 
 class bike:
@@ -32,69 +36,91 @@ class station:
 		self.no_pedalbikes = no_pedalbikes
 		self.bike_list= bike_list
 
-	def UpdateNumberofBikes(self,biketype_assigned):
+	def UpdateNumberofBikes(self,biketype_assigned,kind):
 
 		print("updating the bikes in the station")
-		if biketype_assigned=="pedalbike":
-			self.no_pedalbikes-=self.no_pedalbikes
-		else:
-			self.no_ebikes-=self.no_ebikes
 
-		self.no_empty_docks+=self.no_empty_docks
+		if kind == "pickup":
+			if biketype_assigned=="pedalbike":
+				self.no_pedalbikes-=self.no_pedalbikes
+			else:
+				self.no_ebikes-=self.no_ebikes
+
+			self.no_empty_docks+=self.no_empty_docks
 
 		#removing bike from the station's bikelist is done in give_bike function
+		else: #return a bike
+			if biketype_assigned=="pedalbike":
+				self.no_pedalbikes+=self.no_pedalbikes
+			else:
+				self.no_ebikes+=self.no_ebikes
 
+			self.no_empty_docks-=self.no_empty_docks
+
+station_ids = [31104,31110,31113,31114,31116,31296]
 
 def give_end_station(start_station_id):
 	station_ids = [31104,31110,31113,31114,31116,31296]
 	end_index= station_ids.index(start_station_id)
+	end_station_id =0
 
-	end_station_joint_dist = np.array([[0.089552,0.253731,0.089552,0.199005,0.233831,0.134328],[0.142857,0.133641,0.82949,0.267281,0.119816,0.253456],[0.066986,0.320574,0.100478,0.157895,0.143541,0.210526],[0.205357,0.379464,0.049107,0.102679,0.044643,0.218750],[0.231884,0.239130,0.108696,0.101449,0.086957,0.231884],[0.117885,0.264228,0.191057,0.186992,0.138211,0.101525]])
+	end_station_joint_dist = np.array([[0.089562,0.253732,0.089552,0.199005,0.233831,0.134328],[0.142857,0.133641,0.082949,0.267281,0.119816,0.253456],[0.066986,0.320574,0.100478,0.157895,0.143541,0.210526],[0.205357,0.379464,0.049107,0.102679,0.044643,0.218750],[0.231884,0.239130,0.108696,0.101449,0.086957,0.231884],[0.117887,0.26442,0.191057,0.186992,0.138211,0.101525]])
 
-	cust_end_node_cdf= end_station_joint_dist[index]
+	cust_end_node_cdf_temp= end_station_joint_dist[end_index]
+	cust_end_node_cdf =[]
+	y=0
+	x=0
+	for x in cust_end_node_cdf_temp:
+		y=y+x
+		cust_end_node_cdf.append(y)
 
 	cust_end_node = random.uniform(0,1)
+	# print("end station proba {}".format(cust_end_node))
+
 	if cust_end_node <=cust_end_node_cdf[0]:
-    		end_station_id= 31104
-    	elif cust_end_node > cust_end_node_cdf[0] and cust_end_node <= cust_end_node_cdf[1] :
-    		end_station_id= 31110
-    	elif cust_end_node > cust_end_node_cdf[1] and cust_end_node <= cust_end_node_cdf[2] :
-    		end_station_id= 31113
-    	elif cust_end_node > cust_end_node_cdf[2] and cust_end_node <= cust_end_node_cdf[3] :
-    		end_station_id= 31114
-    	elif cust_end_node > cust_end_node_cdf[3] and cust_end_node <= cust_end_node_cdf[4] :
-    		end_station_id= 31116
-    	elif cust_end_node > cust_end_node_cdf[4] and cust_end_node <= cust_end_node_cdf[5] :
-    		end_station_id= 31296
-	
+			end_station_id= 31104
+
+	elif cust_end_node > cust_end_node_cdf[0] and cust_end_node <= cust_end_node_cdf[1]:
+		end_station_id= 31110
+
+	elif cust_end_node > cust_end_node_cdf[1] and cust_end_node <= cust_end_node_cdf[2]:
+		end_station_id= 31113
+	elif cust_end_node > cust_end_node_cdf[2] and cust_end_node <= cust_end_node_cdf[3]:
+		end_station_id= 31114
+	elif cust_end_node > cust_end_node_cdf[3] and cust_end_node <= cust_end_node_cdf[4]:
+		end_station_id= 31116
+	elif cust_end_node > cust_end_node_cdf[4] and cust_end_node <= cust_end_node_cdf[5]:
+		end_station_id= 31296
+	print("end station_id is {}".format(end_station_id))
 	return end_station_id
 
 # This function gives the duration based on the start and end stations
 def give_duration(bike_type,start_station_id,end_station_id):
-	station_ids = [31104,31110,31113,31114,31116,31296]
+	# station_ids = [31104,31110,31113,31114,31116,31296]
 	start_index= station_ids.index(start_station_id)
 	end_index = station_ids.index(end_station_id)
 
 	#matrix for duration
-	ebike_duration_matrix= np.array([[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10]])
+	ebike_duration_matrix= np.array([[3,2,3,2,5,3],
+		[5,5,3,5,2,3],
+		[5,5,3,5,2,3],
+		[5,5,3,5,2,3],
+		[5,5,3,5,2,3],
+		[5,5,3,5,2,3]])
 
-	pedal_duration_matrix= np.array([[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10],
-		[10,10,10,10,10,10]])
+	pedal_duration_matrix= np.array([[3,2,3,2,5,3],
+		[3,2,3,2,5,3],
+		[3,2,3,2,5,3],
+		[3,2,3,2,5,3],
+		[3,2,3,2,5,3],
+		[3,2,3,2,5,3]])
 
-	return duration_matrix[start_index][end_index]
+	if bike_type == "pedalbike":
+		return pedal_duration_matrix[start_index][end_index]
+	else:
+		return pedal_duration_matrix[start_index][end_index]
 
 def give_bike(stations_list,start_station_id,pref_bike):
-
-	print("assigning bike")
 
 	# check if preferred bike exists in start station
 	for x in stations_list:
@@ -133,102 +159,250 @@ def give_bike(stations_list,start_station_id,pref_bike):
 					y.status="riding"
 					return y
 
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+#===============================================================
+'''
+implementing thinning algorithm
+
+'''
+def give_arrivals():
+
+	SEED = None
+	MAXSEEDSEQ = 100000
+	u1count = 0
+	counter = 0
+
+	# *** OUTPUT: customer_times[] contains customer arrival times tuple (customer #, arrival time (hr))
+
+	## code below
+
+	random.seed(SEED)
+	U1 = [random.uniform(0,1) for i in range(MAXSEEDSEQ)] # contains sequence of random #s U(0,1)
+	poisson1rate = [0] # stores uniform poisson arrival rates lambda == 1
+
+
+	#define inverse transform functions here
+
+	def eq1(x): return (x/0.258)
+	def eq2(x): return (x+3.95)/1.048
+	def eq3(x): return ((x+2.2)/.874)
+	def eq4(x): return ((x+15)/1.626)
+	def eq5(x): return ((x+7.76)/1.282)
+	def eq6(x): return ((x+30.72)/2.28)
+
+	vhrthin = pd.read_csv("thinningdata.csv")
+
+
+	for i in range(24):
+	    test = poisson1rate[i] - np.log(U1[counter])
+	    while (test >= 21.72 and eq6(test) >= 24) or test >= 24: # trim domain/range [0,24) of poisson generated value
+	        counter += 1
+	        test = poisson1rate[i] - np.log(U1[counter])
+
+	    poisson1rate.append(test)
+	    counter += 1
+
+	poisson1rate.pop(0) # in place
+
+
+	#cdf inv equations
+
+	'''
+	eq1: y=.214x + 0.22 inv_range: (0,1.29) [0-5] eq1
+
+	eq2: y=1.048x - 3.95 range: 1.29-6.53 [5-10] eq2
+
+	eq3: y=0.874x - 2.2 range: 6.53-12.65 [10-17] eq3 
+
+	eq4: y=1.626x - 15 range: 12.65-19.156 [17-21] eq4
+
+	eq5: y= 1.282x - 7.76 range: 19.156 - 21.72 [21-23] eq5
+
+	eq6: y=2.28x - 30.72, range: 21.72-24 [23-24] eq6
+	'''
+
+
+	flist = [0,eq1,eq2,eq3,eq4,eq5,eq6]
+
+	# define right sets inverse domain = f(x) range
+	places = [
+	    (1.29, '1'),
+	    (6.53, '2'),
+	    (12.65, '3'),
+	    (19.156, '4'),
+	    (21.72, '5'),
+	    (24, '6')
+	]
+
+	places.sort() # list must be sorted
+	pos_log = []
+
+	for to_find in poisson1rate:
+	    pos = bisect.bisect_right(places, (to_find,))
+	    pos_log.append(pos)
+	  #  print ('%s -> %s' % (to_find, places[pos]))
+
+	times_list = [(poisson1rate[x],places[pos_log[x]]) for x in range(len(pos_log))]
+
+	customer = 1
+	customer_times = []
+
+	for x in range(len(times_list)):
+	    arrtime = flist[int(times_list[x][1][1])](times_list[x][0])
+	    customer_times.append((customer, arrtime))
+	    customer += 1
+
+	# print(customer_times)
+	return customer_times
+
+def give_customers(t,customer_times):
+
+	arrival_times=[]
+	for i in range(len(customer_times)):
+		temp_arrival_time = truncate(customer_times[i][1] * 60)
+		arrival_times.append(temp_arrival_time)
+
+	
+
+	no_cust_at_time= 0
+	for time in arrival_times:
+		if time == t:
+			no_cust_at_time+=1
+
+
+	return no_cust_at_time
+
+
+
+
+#===============================================================
+
+def return_bike(t,cust):
+	for s in stations_list:
+				if s.station_id==cust.end_station_id:
+					if s.no_empty_docks !=0:
+						print("empty dock available to return")
+						s.bike_list.append(cust.bike)
+						s.no_empty_docks-=1
+						cust.bike.current_station=s.station_id
+						cust.bike.status= "stationary"
+						s.UpdateNumberofBikes(cust.bike.bike_type, "return")
+						# end_trip_customer.remove(cust)
+					else:
+						print("no empty dock available, redirecting to another station")
+						for ids in station_ids:
+							if cust.end_station_id==ids:
+								#start station must be the initial end station
+								cust.start_station_id=cust.end_station_id
+								#assign the next station from the list as end station
+								cust.end_station_id = station_ids[station_ids.index(ids)+1]
+								#change duration
+								cust.duration=give_duration(cust.bike.bike_type,cust.start_station_id,cust.end_station_id)
+								cust.end_time=t + cust.duration
+								cust.satisfaction=1
+
+
+
+#===============================================================
+print("creating objects--- infrastructure ---")
+stations_list = []
+bike_list =[]
+tot_bikes= 72
+#dummy numbers
+station_capacities= [10,15,15,10,12,10]
+no_empty_docks= [1,2,1,2,1,2]
+no_pedalbikes= [5,8,9,5,6,4]
+no_ebikes  =[4,5,5,4,6,4]
+
+
+# bike objects
+for j in range(0,6):
+	templist=[]
+	for k in range(1,no_ebikes[j]):
+		b1=bike('e'+str(k), station_ids[j],"ebike", "stationary")
+		templist.append(b1)
+	for m in range(1,no_pedalbikes[j]):
+		b2=bike('p'+str(k+m), station_ids[j],"pedalbike", "stationary")
+		templist.append(b2)
+	bike_list.append(templist)
+
+
+#station objects
+for i in range(0,6):
+	stations_list.append(station(station_ids[i],station_capacities[i],no_empty_docks[i],no_ebikes[i],no_pedalbikes[i], bike_list[i]))
+#==================================================================
 
 def main():
-    print("Hello World!")
+	print("Starting simulation")
+	customer_arrivals = give_arrivals()
 
-    #create pre-existing objects
-    station_ids = [31104,31110,31113,31114,31116,31296]
-    stations_list = []
-    bike_list =[]
-    tot_bikes= 72
-    #dummy numbers
-    station_capacities= [10,15,15,10,12,10]
-    no_empty_docks= [1,2,1,2,1,2]
-    no_pedalbikes= [5,8,9,5,6,4]
-    no_ebikes  =[4,5,5,4,6,4]
+	#create pre-existing objects
+	#==================================================================
+	tot_customers = 0
+	customer_list=[]
+	
+	#start time- simulating every minute
+	for t in range(1,1441):
+		print("time: {}  ".format(t))
 
-    # durations are a 6x6 matrix (how?)
-    pedal_trip_duration= 15
-    ebike_trip_duration= 10
+		#create customer objects based on poisson arrival
+		cust_at_time = give_customers(t,customer_arrivals)
+		# for every customer spawning new in the system start the trip
+		for c in range(1,cust_at_time+1):
+			print("==================================")
+			# casual vs member (0.2810 vs 0.7189)
+			casual_member= random.uniform(0,1)
+			if casual_member>= 0.718929:
+				cust_type= "casual"
+			else:
+				cust_type ="member"
+			print("customer type is {}".format(cust_type))
 
-    # bike objects
-    for j in range(0,6):
-    	templist=[]
-    	for k in range(1,no_ebikes[j]):
-    		b1=bike('e'+k, station_ids[j],"ebike", "stationary")
-    		templist.append(b1)
-    	for m in range(1,no_pedalbikes[j]):
-    		b2=bike('p'+k+m, station_ids[j],"pedalbike", "stationary")
-    		templist.append(b2)
-    	bike_list.append(templist)
+			#assign customer to  start station and end station based on priors
+			cust_start_node = random.uniform(0,1)
+			cust_start_node_cdf = [0.162753,0.338462,0.507693,0.689070,0.800811,1]
 
+			if cust_start_node <=cust_start_node_cdf[0]:
+				start_station_id= 31104
+			elif cust_start_node > cust_start_node_cdf[0] and cust_start_node <= cust_start_node_cdf[1] :
+				start_station_id= 31110
+			elif cust_start_node > cust_start_node_cdf[1] and cust_start_node <= cust_start_node_cdf[2] :
+				start_station_id= 31113
+			elif cust_start_node > cust_start_node_cdf[2] and cust_start_node <= cust_start_node_cdf[3] :
+				start_station_id= 31114
+			elif cust_start_node > cust_start_node_cdf[3] and cust_start_node <= cust_start_node_cdf[4] :
+				start_station_id= 31116
+			elif cust_start_node > cust_start_node_cdf[4] and cust_start_node <= cust_start_node_cdf[5] :
+				start_station_id= 31296
+			print("start_station_id: {}".format(start_station_id))
 
-    #station objects
-    for i in range(0,6):
-    	stations_list.append(station(station_ids[i],station_capacities[i],no_empty_docks[i],no_ebikes[i],no_pedalbikes[i], bike_list[i]))
+			#assign end station based on joint probabilities
+			end_station_id = give_end_station(start_station_id)
 
-    #==================================================================
-    tot_customers = 0
-    customer_list=[]
-    #start time- simulating every minute
-    for t in range(1,1441):
-    	print("time: {}  ".format(t))
+			#assign bike preference based on priors
+			pedal_ebike= random.uniform(0,1)
+			if pedal_ebike>= 0.881335:
+				pref_bike= "pedal"
+			else:
+				pref_bike ="ebike"
 
-    	#create customer objects based on poisson arrival
-	    cust_at_time = 5 # set poisson
+			#check if preferred bike exists in station else assign alternate bike
+			bike_assigned= give_bike(stations_list,start_station_id,pref_bike)
+			print("bike assigned is {}".format(bike_assigned.bike_type))
+			#duration based on fixed average durations between stations
+			trip_duration= give_duration(bike_assigned.bike_type,start_station_id,end_station_id)
+			print("trip_duration is {}".format(trip_duration))
 
-	    # for every customer spawning new in the system start the trip
-	    for c in range(1,cust_at_time+1):
+			#update customer object with all the calculated fields
+			customer_list.append(customer(tot_customers+c,cust_type,bike_assigned,t,t+trip_duration,trip_duration,start_station_id,end_station_id,0))
 
-	    	# casual vs member (0.2810 vs 0.7189)
-	    	casual_member= random.uniform(0,1)
-	    	if casual_member>= 0.718929:
-	    		cust_type= "casual"
-	    	else:
-	    		cust_type ="member"
-
-	    	#assign customer to  start station and end station based on priors
-	    	cust_start_node = random.uniform(0,1)
-	    	cust_start_node_cdf = [0.162753,0.338462,0.507693,0.689070,0.800811,1]
-
-	    	if cust_start_node <=cust_start_node_cdf[0]:
-	    		start_station_id= 31104
-	    	elif cust_start_node > cust_start_node_cdf[0] and cust_start_node <= cust_start_node_cdf[1] :
-	    		start_station_id= 31110
-	    	elif cust_start_node > cust_start_node_cdf[1] and cust_start_node <= cust_start_node_cdf[2] :
-	    		start_station_id= 31113
-	    	elif cust_start_node > cust_start_node_cdf[2] and cust_start_node <= cust_start_node_cdf[3] :
-	    		start_station_id= 31114
-	    	elif cust_start_node > cust_start_node_cdf[3] and cust_start_node <= cust_start_node_cdf[4] :
-	    		start_station_id= 31116
-	    	elif cust_start_node > cust_start_node_cdf[4] and cust_start_node <= cust_start_node_cdf[5] :
-	    		start_station_id= 31296
-
-	    	#assign end station based on joint probabilities
-	    	end_station_id = give_end_station(start_station_id)
-
-	    	#assign bike preference based on priors
-	    	pedal_ebike= random.uniform(0,1)
-	    	if pedal_ebike>= 0.881335:
-	    		pref_bike= "pedal"
-	    	else:
-	    		pref_bike ="ebike"
-
-	    	#check if preferred bike exists in station else assign alternate bike
-	    	bike_assigned= give_bike(stations_list,start_station_id,pref_bike)
-
-	    	#duration based on fixed average durations between stations
-	    	trip_duration= give_duration(biketype_assigned.bike_type,start_station_id,end_station_id)
-
-	    	#update customer object with all the calculated fields
-	    	customer_list.append(customer(tot_customers+c,cust_type,bike_assigned,t,t+duration,duration,start_station_id,end_station_id))
-
-	    	#update bike in start station
-	    	#call update bike function for the station object
-	    	for x in stations_list:
+			#update bike in start station
+			#call update bike function for the station object
+			for x in stations_list:
 				if start_station_id == x.station_id:
-					x.UpdateNumberofBikes(bike_assigned.bike_type)
+					x.UpdateNumberofBikes(bike_assigned.bike_type,"pickup")
 
 
 		# for customers who are currently in the middle of the trip, check if their trip is over.
@@ -239,32 +413,16 @@ def main():
 				end_trip_customer.append(cust)
 
 
-	    #end trip- check if there are empty docks, else direct to nearest station with empty dock
-	    for cust in end_trip_customer:
-	    	for s in stations_list:
-	    		if s.station_id==cust.end_station_id:
-	    			if s.no_empty_docks !=0:
-	    				print("empty dock available to return")
-	    				s.bike_list.append(cust.bike)
-	    				s.no_empty_docks-=1
-	    				cust.bike.current_station=s.station_id
-	    				cust.bike.status= "stationary"
-	    				end_trip_customer.remove(cust)
-	    			else:
-	    				print("no empty dock available, redirecting to another station")
-	    				for ids in station_ids:
-	    					if cust.end_station_id==ids:
-	    						cust.end_station_id = station_ids[station_ids.index(ids)+1]
+		#end trip- check if there are empty docks, else direct to nearest station with empty dock
+		for cust in end_trip_customer:
+			return_bike(t,cust)
 
 
-
-
-
-	    #update bike in end station
-	    tot_customers= tot_customers+cust_at_time
+		#update bike in end station
+		tot_customers= tot_customers+cust_at_time
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
 
